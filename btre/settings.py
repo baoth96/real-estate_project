e7788bce8a32026d5c8d8d8b0441f698b518ea80
baoth96 <thaihoangbao112@gmail.com>
2022-12-22 15:21:45 +0700

    Initial commit

--- btre/settings.py
+++ btre/settings.py
@@ -138,6 +138,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
